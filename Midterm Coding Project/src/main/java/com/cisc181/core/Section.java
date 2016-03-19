01ad20386f80c4e41fe04aac8f08e8de1642b5fb
David Heck <dheck@udel.edu>
2016-03-19 05:50:45 -0400

    Classes Almost Complete.

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/Section.java
@@ -0,0 +1,48 @@
+package com.cisc181.core;
+
+import java.util.UUID;
+
+public class Section {
+	
+	//Attributes
+	private UUID SemeseterID;
+	private UUID CourseID;
+	private UUID SectionID;
+	private int RoomID;
+	
+	//Getters and Setters
+	public UUID getSemeseterID() {
+		return SemeseterID;
+	}
+	public void setSemeseterID(UUID semeseterID) {
+		SemeseterID = semeseterID;
+	}
+	public UUID getCourseID() {
+		return CourseID;
+	}
+	public void setCourseID(UUID courseID) {
+		CourseID = courseID;
+	}
+	public UUID getSectionID() {
+		return SectionID;
+	}
+	public void setSectionID(UUID sectionID) {
+		SectionID = sectionID;
+	}
+	public int getRoomID() {
+		return RoomID;
+	}
+	public void setRoomID(int roomID) {
+		RoomID = roomID;
+	}
+
+	//Constructor
+	public Section(UUID semeseterID, UUID courseID, UUID sectionID, int roomID) {
+		super();
+		SemeseterID = semeseterID;
+		CourseID = courseID;
+		SectionID = sectionID;
+		RoomID = roomID;
+	}
+	
+}
